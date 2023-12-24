3557dddc305f3ff47f7fdd13dc9e0c421c7038be
NicoOhR <nimoohayonr@gmail.com>
2023-12-24 15:24:44 -0600

    respec Alpha to return bytestring response

--- app/Alpha.hs
+++ app/Alpha.hs
@@ -12,14 +12,15 @@ import Data.Monoid ((<>))
 import Data.Text (Text)
 import GHC.Generics
 import Network.HTTP.Req
+import Network.HTTP.Simple as HTTP
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Text.URI as URI
 
-alpha :: String -> IO ()
+alpha :: String -> IO BL.ByteString
 alpha t = runReq defaultHttpConfig $ do
    let url = https "alphavantage.co" /: "query"
    let queryParam = "function" =: ("TIME_SERIES_MONTHLY" :: Text)
                   <> "symbol" =: t 
                   <> "apikey" =: ("AEZLT4ZC9SZ9MUK1" :: Text)
-   r <- req GET url NoReqBody jsonResponse queryParam
-   liftIO $ BL.putStrLn (encodePretty (responseBody r :: Value))
+   r <- req GET url NoReqBody lbsResponse queryParam
+   return $ responseBody r
